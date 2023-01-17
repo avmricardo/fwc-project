a89efccd005752fa2fd405fea9aaf119ba89b290
Ricardo Augusto Valle Maciel <ricardomaciel.unb@gmail.com>
2023-01-17 18:37:07 -0300

    tirando divisao das fases da copa do datas.py

--- datas.py
+++ datas.py
@@ -5,18 +5,6 @@ import pandas as pd
 
 data = pd.read_csv("FWC_data.csv")
 
-group_stage = data[:48]
-round_of_16 = data[48:56]
-round_of_16.index = range(8)
-quarter_finals = data[56:60]
-quarter_finals.index = range(4)
-semi_finals = data[60:62]
-semi_finals.index = range(2)
-third_place = data[62:63]
-third_place.index = range(1)
-final = data[63:]
-final.index = range(1)
-
 groupA = ["QATAR", "ECUADOR", "SENEGAL", "NETHERLANDS"]
 groupB = ["ENGLAND", "IRAN", "UNITED STATES", "WALES"]
 groupC = ["ARGENTINA", "SAUDI ARABIA", "MEXICO", "POLAND"]
